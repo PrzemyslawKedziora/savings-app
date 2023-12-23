4e5d3ac586b17da8a694453d2224008c79a76582
blancior <pkedziora347@gmail.com>
2023-12-23 20:08:58 +0100

    Create: created user controller

--- server/laravel/app/Models/User.php
+++ server/laravel/app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'budgetLimit',
+        'savingsGoal',
+        'balance'
     ];
 
     /**
@@ -41,4 +44,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function records()
+    {
+        return $this->hasMany(Record::class, 'userID');
+    }
 }
--- server/laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->double('budgetLimit');
+            $table->double('savingsGoal');
+            $table->double('balance');
             $table->rememberToken();
             $table->timestamps();
         });
