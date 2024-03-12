d28670646e6c335644cfa64575e96cf18904f816
blancior <pkedziora347@gmail.com>
2024-03-12 21:06:33 +0100

    Feat: sorting system at transactions component

--- client/src/app/components/transactions/transactions.component.ts
+++ client/src/app/components/transactions/transactions.component.ts
@@ -4,6 +4,8 @@ import {RecordModel} from "../../models/record.model";
 import {RecordItemDetailedComponent} from "../record/record-item-detailed/record-item-detailed.component";
 import {MatPaginator} from "@angular/material/paginator";
 import {RecordItemComponent} from "../record/record-item/record-item.component";
+import {MatDialog} from "@angular/material/dialog";
+import {NewRecordComponent} from "../record/new-record/new-record.component";
 
 @Component({
   selector: 'app-transactions',
@@ -19,8 +21,10 @@ import {RecordItemComponent} from "../record/record-item/record-item.component";
 export class TransactionsComponent implements OnInit{
   records$!: RecordModel[];
   subscriptions$!:RecordModel[];
+  private sortDirection: number = 1;
   constructor(
-    private recordService: RecordService
+    private recordService: RecordService,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -30,6 +34,35 @@ export class TransactionsComponent implements OnInit{
     })
   }
 
+  sortby(criteria: string,tab:RecordModel[]){
+    switch (criteria) {
+      case 'name':{
+        tab.sort((record1,record2)=>{
+          return this.sortDirection * (record1.description.localeCompare(record2.description));
+        })
+        break;
+      }
+      case 'date':{
+        tab.sort((record1,record2)=>{
+          const date1 = new Date(record1.updated_at);
+          const date2 = new Date(record2.updated_at);
+
+          return this.sortDirection * (date1.getTime() - date2.getTime());
+        })
+        break;
+      }
+      case 'amount':{
+        tab.sort((record1,record2)=>{
+            return this.sortDirection * (record1.money_spent - record2.money_spent);
+        })
+        break;
+      }
+    }
+    this.sortDirection *= -1;
+  }
+  openAddRecordDialog(){
+    this.dialog.open(NewRecordComponent);
+  }
 
 
 }
